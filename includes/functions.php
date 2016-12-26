b2836be307a4ca6d84e6e3195e6be8f9145ea1d7
Simon Prosser <pross@pross.org.uk>
2016-12-26 18:49:56 +0000

    Better time_elapsed_string() function

--- includes/functions.php
+++ includes/functions.php
@@ -145,6 +145,35 @@ function conTime($secs) {
     return $val;
 }
 
+function time_elapsed_string($datetime, $full = false) {
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
+
 function truncate($input, $maxWords, $maxChars)
 {
     $words = preg_split('/\s+/', $input);
