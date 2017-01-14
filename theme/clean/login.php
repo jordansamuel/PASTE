2999aeb35d42c31b4b60cbce410ff51cd0d8c22f
J Samuel <jordan@boxlabs.co.uk>
2017-01-14 13:11:26 +0100

    ..

--- theme/clean/login.php
+++ theme/clean/login.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Paste <https://github.com/jordansamuel/PASTE> - Default theme
+ * Paste <https://github.com/jordansamuel/PASTE> - Clean theme
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
