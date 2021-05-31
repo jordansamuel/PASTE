9b885993c0b3215e2b3e84e16bd3634310d0cceb
bunder2015 <omfgbunder@gmail.com>
2021-05-31 10:37:48 -0400

    round conTime for new posts up to 1 second
    Fixes #164
    
    Signed-off-by: bunder2015 <omfgbunder@gmail.com>

--- includes/functions.php
+++ includes/functions.php
@@ -113,6 +113,11 @@ function updateMyView($con, $paste_id)
 }
 
 function conTime($secs) {
+    // round up to 1 second
+    if ($secs == 0) {
+        $secs = 1;
+    }
+
     $bit = array(
         ' year' => $secs / 31556926 % 12,
         ' week' => $secs / 604800 % 52,
